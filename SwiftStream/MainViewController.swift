beebf44d8870b36832a9901f6db1aeded5a38c4e
Frederick C. Lee <frederick.c.lee@gmail.com>
2018-01-11 19:21:46 -0800

    Tweaked 'My Project' label placement.

--- SwiftStream/MainViewController.swift
+++ SwiftStream/MainViewController.swift
@@ -9,9 +9,30 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
+    
+    let titleLabel:UILabel = {
+        var label = UILabel()
+        label.text = "My Project"
+        label.textColor = UIColor.purple
+        label.font = UIFont(name: "Noteworthy", size: 23.0)
+        label.textAlignment = .center
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubview(titleLabel)
+        titleLabel.anchor(top: view.safeAreaLayoutGuide.topAnchor,
+                          bottom: nil,
+                          left: nil,
+                          right: nil,
+                          centerYAnchor: nil,
+                          centerXAnchor: view.safeAreaLayoutGuide.centerXAnchor,
+                          paddingTop: -130,
+                          paddingLeft: 10,
+                          paddingBottom: 10,
+                          paddingRight: 10, width: 100.0, height: 320)
+        
         view.backgroundColor = UIColor.skyBlue()
         self.title = "Swift Stream"
     }
